eb8f1dd08b56ebe056cccc8403ed2f2ddd1ceae3
Josiah Worcester <josiahw@gmail.com>
2012-09-09 21:06:21 -0600

    Switch to gcc 4.2.1

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,27 +2,22 @@
 
 export PATH=$(pwd)/cross/bin:$(pwd)/utils:$PATH
 
+: ${A:=$(uname -m)}
+
 build cross-scripts/binutils-*
 build cross-scripts/gcc-*
 
-export CC=""
+export CC="$A-unknown-linux-musl-gcc"
 export CFLAGS="-Os"
 export LDFLAGS="-s"
 
 build cross-scripts/linux-headers-*
 build cross-scripts/musl-*
 
-build cross-scripts/gmp-*
-build cross-scripts/mpfr-*
-build cross-scripts/mpc-*
-
 build build-scripts/musl-*
 build build-scripts/binutils-*
 build build-scripts/gcc-*
 build build-scripts/make-*
-build build-scripts/gmp-*
-build build-scripts/mpfr-*
-build build-scripts/mpc-*
 build build-scripts/busybox-*
 build build-scripts/linux-*
 build build-scripts/finish
