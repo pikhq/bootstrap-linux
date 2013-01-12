553634c42c7e0874f34b91fa01203ebe96666d5f
Josiah Worcester <josiahw@gmail.com>
2013-01-12 14:32:55 -0600

    Made the make_fs script much faster

--- make_fs.sh
+++ make_fs.sh
@@ -1,2 +1,4 @@
 #!/bin/sh -x
-genext2fs -d out/ -m 5 -N 61440 -U -b 2097152 filesystem.img
+BLOCKS=$(((1024*$(du -m -s out | awk '{print $1}')*12)/10))
+genext2fs -z -d out/ -U -i 1024 -b $BLOCKS filesystem.img
+resize2fs filesystem.img 500M
